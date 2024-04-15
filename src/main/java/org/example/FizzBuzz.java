b9b9ab53d85e12103386a2bd957ef1767fc7a6b1
Administrator <brigittehounkpe@cmail.carleton.ca>
2024-04-14 23:58:37 -0400

    Version3 : Success Test FizzBuzz with 0, Success Test FizzBuzz with 1

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -7,6 +7,11 @@ package org.example;
 public class FizzBuzz
 {
     public static String fizzBuzz(int num){
-        return "0";
+        if (num == 1){
+            return "1";
+        }else{
+            return "0";
+        }
+
     }
 }
