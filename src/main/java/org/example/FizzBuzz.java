bb49476515e0ccf385290df9004af21172420a77
Administrator <brigittehounkpe@cmail.carleton.ca>
2024-04-15 00:05:52 -0400

    Version5 : Success Test FizzBuzz with 0, Success Test FizzBuzz with 1, Success Test FizzBuzz 0 to 50

--- src/main/java/org/example/FizzBuzz.java
+++ src/main/java/org/example/FizzBuzz.java
@@ -9,9 +9,19 @@ public class FizzBuzz
     public static String fizzBuzz(int num){
         if (num == 1){
             return "1";
-        }else{
+        }else if (num == 0){
             return "0";
-        }
+        }else{
+            if (num%15 == 0){
+                return "FizzBuzz";
+            }else if (num%5 == 0){
+                return "Buzz";
+            }else if (num%3 == 0){
+                return "Fizz";
+            }else{
+                return Integer.toString(num);
+            }
 
+        }
     }
 }
--- src/test/java/org/example/FizzBuzzTest.java
+++ src/test/java/org/example/FizzBuzzTest.java
@@ -5,25 +5,33 @@ import static org.junit.Assert.*;
 
 
 public class FizzBuzzTest {
-    String [] strFizzBuzzAnswer;
+    String[] strFizzBuzzAnswer;
 
     @Before
-    public void setUpAnswer(){
-        strFizzBuzzAnswer = new String[] {"0","1","2","Fizz","4","Buzz","Fizz","7","8","Fizz",
-                                            "Buzz","11","Fizz","13","14","FizzBuzz","16","17","Fizz","19",
-                                            "Buzz","Fizz","22","23","Fizz","Buzz","26","Fizz","28","29",
-                                            "FizzBuzz","31","32","Fizz","34","Buzz","Fizz","37","38","Fizz",
-                                            "Buzz","41","Fizz","43","44","FizzBuzz","46","47","Fizz","49",
-                                          };
+    public void setUpAnswer() {
+        strFizzBuzzAnswer = new String[]{"0", "1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8", "Fizz",
+                "Buzz", "11", "Fizz", "13", "14", "FizzBuzz", "16", "17", "Fizz", "19",
+                "Buzz", "Fizz", "22", "23", "Fizz", "Buzz", "26", "Fizz", "28", "29",
+                "FizzBuzz", "31", "32", "Fizz", "34", "Buzz", "Fizz", "37", "38", "Fizz",
+                "Buzz", "41", "Fizz", "43", "44", "FizzBuzz", "46", "47", "Fizz", "49",
+        };
     }
 
     @Test
-    public void testFizzBuzz0(){
-       assertEquals(FizzBuzz.fizzBuzz(0),strFizzBuzzAnswer[0]);
+    public void testFizzBuzz0() {
+        assertEquals(FizzBuzz.fizzBuzz(0), strFizzBuzzAnswer[0]);
     }
 
     @Test
-    public void testFizzBuzz1(){
-        assertEquals(FizzBuzz.fizzBuzz(1),strFizzBuzzAnswer[1]);
+    public void testFizzBuzz1() {
+        assertEquals(FizzBuzz.fizzBuzz(1), strFizzBuzzAnswer[1]);
     }
+
+    @Test
+    public void testFizzBuzz0To50(){
+        for (int i = 0; i<50;i++){
+            assertEquals(FizzBuzz.fizzBuzz(i), strFizzBuzzAnswer[i]);
+        }
+    }
+
 }
